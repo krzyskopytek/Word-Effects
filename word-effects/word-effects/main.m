eac4901bdffd959e398353ada7a5bef344d03aba
krzyskopytek <krzys.kopytek@hotmail.com>
2016-05-02 18:19:15 -0400

    init

---
+++ word-effects/word-effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  word-effects
+//
+//  Created by Krzysztof Kopytek on 2016-05-02.
+//  Copyright © 2016 Krzysztof Kopytek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello");
+    }
+    return 0;
+}
